c88dbab096e51db5fe39c3df9647d5cf49aab4d7
nadeysha <nadeysha@mail.ru>
2021-10-18 19:06:46 +0400

    feat: add queue solution

--- src/st-queue.js
+++ src/st-queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,20 +14,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 module.exports = class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
-  dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
-
+constructor(){
+  this.items = []
 }
+ getUnderlyingList(){
+  return this.items
+}
+   
+  
+
+enqueue (item) {
+  this.items.push(item)
+}
+
+
+dequeue ()  {
+ return  this.items.shift()
+}
+
+peek() {
+  return this.items[0]
+}
+}
+
